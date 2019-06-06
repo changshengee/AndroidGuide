50c74ca58d395b653cb123eed8135c2c6f0569dc
changshengee <1010875980@qq.com>
2019-06-06 13:32:42 +0800

    :bento: 添加两个按钮

--- app/src/main/java/changsheng/com/geoquiz/QuizActivity.java
+++ app/src/main/java/changsheng/com/geoquiz/QuizActivity.java
@@ -1,13 +1,22 @@
 package changsheng.com.geoquiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
+/**
+ * @author changshengee
+ */
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mTrueButton =  findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
     }
 }
