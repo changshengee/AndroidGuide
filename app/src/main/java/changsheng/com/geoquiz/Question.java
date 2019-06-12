4d1fd6d1d3726c13ba9dc7aa71bcc9a4690404f4
changshengee <1010875980@qq.com>
2019-06-12 19:15:02 +0800

    :sparkles:  添加question

--- app/src/main/java/changsheng/com/geoquiz/Question.java
+++ app/src/main/java/changsheng/com/geoquiz/Question.java
@@ -1,4 +1,37 @@
 package changsheng.com.geoquiz;
 
+/**
+ * @author changshengee
+ */
 public class Question {
+    /**
+     * 问题id
+     */
+    private int mTextResId;
+
+    /**
+     * 答案
+     */
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
 }
--- app/src/main/java/changsheng/com/geoquiz/QuizActivity.java
+++ app/src/main/java/changsheng/com/geoquiz/QuizActivity.java
@@ -2,7 +2,10 @@ package changsheng.com.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * @author changshengee
@@ -11,12 +14,40 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int currentIndex = 0;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        mTrueButton =  findViewById(R.id.true_button);
+        mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
+        mNextButton = findViewById(R.id.next_button);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+        int question=mQuestionBank[currentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
