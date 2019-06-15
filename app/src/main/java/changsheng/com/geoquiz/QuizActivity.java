153ea1c02a0bdbee603044be03ccbacabdd9512d
changshengee <1010875980@qq.com>
2019-06-15 18:35:09 +0800

    :sparkles: 添加水平布局

--- app/src/main/java/changsheng/com/geoquiz/QuizActivity.java
+++ app/src/main/java/changsheng/com/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package changsheng.com.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,18 +19,20 @@ public class QuizActivity extends AppCompatActivity {
     private TextView mQuestionTextView;
     private int currentIndex = 0;
 
+    public static final String TAG = "QuizActivity";
+
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
             new Question(R.string.question_africa, false),
             new Question(R.string.question_americas, true),
             new Question(R.string.question_asia, true)
-
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton = findViewById(R.id.false_button);
@@ -56,6 +59,44 @@ public class QuizActivity extends AppCompatActivity {
                         updateQuestion();
                     }
                 });
+        mQuestionTextView.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        currentIndex = (currentIndex + 1) % mQuestionBank.length;
+                        updateQuestion();
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion() {
