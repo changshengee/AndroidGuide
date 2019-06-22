7b722a6274b3b65c502d053443aae468ccb9229b
changshengee <1010875980@qq.com>
2019-06-22 11:11:50 +0800

    :wrench: 修改作弊代码

--- app/src/main/java/changsheng/com/geoquiz/CheatActivity.java
+++ app/src/main/java/changsheng/com/geoquiz/CheatActivity.java
@@ -1,10 +1,13 @@
 package changsheng.com.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -65,6 +68,18 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 mIsAnswerShown = true;
                 setAnswerShownResult();
+                int cx = mShowAnswer.getWidth() / 2;
+                int cy = mShowAnswer.getHeight() / 2;
+                float radius = mShowAnswer.getWidth();
+                Animator animator = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                animator.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mShowAnswer.setVisibility(View.INVISIBLE);
+                    }
+                });
+                animator.start();
             }
         });
     }
